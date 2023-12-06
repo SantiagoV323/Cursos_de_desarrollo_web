40652f17dc2e49da1cd202628b7f048ff78e7d13
Santiago Vega F <santiago_vega99@hotmail.com>
2023-12-05 22:30:26 -0500

    Creado documento de explicación de variables

---
+++ Frontend-developer/Curso básico de JavaScript/Clase-4-Variables.js
@@ -0,0 +1,23 @@
+    //Crear e inicializar una variable
+var nombre = "Santiago";
+
+
+    //Crear variable
+var edad;
+
+
+    //Inicializar variable
+edad = 24;
+
+
+    //Crear variables de tipo array
+var elementos = ["Computador", "Celular"];
+        //Acceder a los elementos del array
+elementos [1]
+
+
+    //Crear variables tipo objeto
+var persona = {
+    nombre: "Andres",
+    edad : 24
+}
