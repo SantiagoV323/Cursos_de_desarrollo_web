ccadb44dc903bff26040df840fe7c83daefe102d
Santiago Vega F <santiago_vega99@hotmail.com>
2023-12-14 00:04:12 -0500

    Mejorada la explicación del ejercicio acerca de Switch

--- Frontend-developer/Curso básico de JavaScript/Clase-15-Switch.js
+++ Frontend-developer/Curso básico de JavaScript/Clase-15-Switch.js
@@ -6,13 +6,13 @@
 
 var numero = 1;
 
-switch (true) {
-    case 1:
-        console.log("Hola, soy uno");
-        break;
-    case 2:
+switch (numero) {   // Dentro de los paréntesis va la condición que queremos evaluar. En este caso, queremos evaluar si la variable numero es igual a 1.
+    case 1: // En caso de que el dato sea igual a 1, se ejecuta el bloque de código que está debajo.
+        console.log("Hola, soy uno"); // Si la condición se cumple, se ejecuta este bloque de código.
+        break; // El break sirve para indicar que el bloque de código terminó.
+    case 2: // En caso de que el dato sea igual a 2, se ejecuta el bloque de código que está debajo.
         console.log("Hola, soy 2");
         break;
-    default:
-        console.log("Hola, soy default");
+    default: // El default se ejecuta en caso de que ninguno de los casos anteriores se cumpla.
+        console.log("Hola, soy default"); // Si la condición no se cumple, se ejecuta este bloque de código.
 }
