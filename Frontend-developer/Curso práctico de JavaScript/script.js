f732ba7011268d865d4717be1fb374132dda2695
Santiago Vega F <santiago_vega99@hotmail.com>
2023-12-21 02:55:29 -0500

    Clase #12, explicación de cómo leer código HTML desde JS

--- Frontend-developer/Curso práctico de JavaScript/script.js
+++ Frontend-developer/Curso práctico de JavaScript/script.js
@@ -1,1 +1,17 @@
-console.log('Hola mundo')
+// El querySelector nos permite seleccionar un elemento de nuestro HTML, en este caso seleccionamos el h1, es similar a como se hace en CSS.
+
+const h1 = document.querySelector('h1');
+const p = document.querySelector('p');
+const prrf = document.querySelector('.prrf');
+const pid = document.querySelector('#pid'); // se usa # para seleccionar por id
+const input = document.querySelector('input');
+
+console.log({ // se usa {} para imprimir varios elementos como si se tratara de un objeto
+    h1,
+    p,
+    prrf,
+    pid,
+    input
+});
+
+console.log(input.value); // se usa .value para obtener el valor de un input
